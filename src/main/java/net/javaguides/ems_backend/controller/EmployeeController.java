ac03f1d8dd0aff2c2d78602d09c15782376dbfdd
banushasarma <sharmabanu03@gmail.com>
2024-06-14 22:02:31 +0530

    changes in create employee

--- src/main/java/net/javaguides/ems_backend/controller/EmployeeController.java
+++ src/main/java/net/javaguides/ems_backend/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import net.javaguides.ems_backend.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,7 @@ public class EmployeeController {
 
     //Build Add Employee REST API
     @PostMapping
-    public ResponseEntity<EmployeeDto> createEmployee(EmployeeDto employeeDto) {
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
         EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
         return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
